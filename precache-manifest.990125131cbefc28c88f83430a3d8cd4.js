f546174aad494c1446ab58acb8a8a761cf0a269e
Sophia Madar <spmadar@gmail.com>
2020-05-14 18:07:33 -0400

    Updates

---
+++ precache-manifest.990125131cbefc28c88f83430a3d8cd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cd0f8e3e8dc433e2de479b351b5062",
+    "url": "/todo-app-project/index.html"
+  },
+  {
+    "revision": "ccf586f7a1fda3f717bd",
+    "url": "/todo-app-project/static/css/main.07455b9b.chunk.css"
+  },
+  {
+    "revision": "434938fb1da6bf8b7429",
+    "url": "/todo-app-project/static/js/2.0f6e200f.chunk.js"
+  },
+  {
+    "revision": "0561bab322d3719c8697d70497653596",
+    "url": "/todo-app-project/static/js/2.0f6e200f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf586f7a1fda3f717bd",
+    "url": "/todo-app-project/static/js/main.a39cb002.chunk.js"
+  },
+  {
+    "revision": "90706672e4fd94664c6b",
+    "url": "/todo-app-project/static/js/runtime-main.d5515607.js"
+  }
+]);
